861bc95a0391d1add2cd7f6d44f70e1736f3a66c
Nguyen Hieu <tthzicc@gmail.com>
2022-05-16 15:42:03 +0700

    Update put user

--- src/api/controllers/user.controller.js
+++ src/api/controllers/user.controller.js
@@ -1,5 +1,8 @@
 const User = require("../../models/user.model");
 const Post = require("../../models/post.model");
+const multer = require("multer");
+const upload = multer({ dest: "src/public/uploads/" });
+const cloudinary = require("cloudinary").v2;
 
 module.exports.getUser = async (req, res) => {
   const user = await User.findOne({ _id: req.params.id });
@@ -14,4 +17,40 @@ module.exports.getUser = async (req, res) => {
   }).sort({ _id: -1 });
 
   res.status(200).send({ user, posts });
+};
+
+module.exports.updateUser = async (req, res) => {
+  const urls = [];
+  const { files } = req;
+  const hasBlockField = typeof req.body.isBlocked !== "undefined";
+
+  // check if files is an array
+  if (files && typeof files === "object") {
+    for (const file of files) {
+      const { url } = await cloudinary.uploader.upload(file.path);
+      urls.push(url);
+    }
+  }
+  // if files is a string
+  else if (files) {
+    const { url } = await cloudinary.uploader.upload(files);
+    urls.push(url);
+  }
+
+  if (hasBlockField) {
+    return res.status(403).send("Forbidden");
+  }
+
+  await User.updateOne(
+    {
+      _id: req.params.userId,
+    },
+    {
+      ...req.body,
+      avatar: urls[0],
+      updatedAt: new Date(),
+    }
+  );
+
+  res.status(200).send("Cap nhap thanh cong!");
 };
--- src/api/routes/user.route.js
+++ src/api/routes/user.route.js
@@ -1,7 +1,11 @@
 const router = require("express").Router();
+const multer = require("multer");
+const upload = multer({ dest: "src/public/uploads/" });
 
 const apiUserController = require("../controllers/user.controller");
 
 router.get("/:id", apiUserController.getUser);
+
+router.put("/:userId", upload.array("photos", 6), apiUserController.updateUser);
 
 module.exports = router;
