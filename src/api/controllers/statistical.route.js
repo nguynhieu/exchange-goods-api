fb506bda298aa43e56325fa1b1f3f3a45ef0dbd6
Nguyen Hieu <tthzicc@gmail.com>
2022-05-16 14:02:04 +0700

    Update statistical

--- src/api/controllers/statistical.route.js
+++ src/api/controllers/statistical.route.js
@@ -1,10 +1,27 @@
 const Statistical = require("../../models/statistical.model");
 
+const generateStatistical = (year, month) => {
+  const months = Array.from(Array(12).keys());
+
+  return {
+    year,
+    statistics: months.map((m) => ({
+      month: m,
+      total: m === month ? 1 : 0,
+    })),
+  };
+};
+
 module.exports.index = async (req, res) => {
   const { year } = req.query;
 
-  const statistics = await Statistical.find(year ? { year } : {}).sort({
+  const statistics = await Statistical.findOne(year ? { year } : {}).sort({
     _id: -1,
   });
-  res.json({ data: statistics });
+
+  if (statistics.length === 0) {
+    return res.json(generateStatistical(year));
+  }
+
+  res.json(statistics);
 };
